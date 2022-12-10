6b6f158af681528ce4b09e479d91709f412438dd
McDony Lee <mcdonyleee@gmail.com>
2022-12-11 03:03:01 +1100

    Added movies & shows search, add no-poster img, and updated logo on project 17: move app

--- 17 - Movie App/script.js
+++ 17 - Movie App/script.js
@@ -2,7 +2,7 @@ const API_URL =
     "https://api.themoviedb.org/3/discover/movie/?api_key=888ab77b38486a7f9bb6dbb1dc6679f1&page1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
-    "https://api.themoviedb.org/3/search/movie?api_key=888ab77b38486a7f9bb6dbb1dc6679f1&query=";
+    "https://api.themoviedb.org/3/search/multi?api_key=888ab77b38486a7f9bb6dbb1dc6679f1&query=";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
@@ -28,17 +28,28 @@ function showMovies(movies) {
     main.innerHTML = ""; //clear it
     movies.forEach((movie) => {
         //object destructing to get specific key from movie
-        const { title, poster_path, vote_average, overview } = movie;
+        const { title, poster_path, vote_average, overview, name } = movie;
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
 
+        //poster conditional if not found
+        let posterPath = "";
+        if (poster_path) {
+            posterPath = IMG_PATH + poster_path;
+        } else {
+            posterPath = "no-poster.png";
+        }
+
+        //title conditional if not found
+        if (title) titleName = title;
+        else titleName = name;
         movieEl.innerHTML = `
         <img
-            class="poster" src="${IMG_PATH + poster_path}" alt="${title}"
+            class="poster" src="${posterPath}" alt="${title}"
         />
         <div class="movie-info">
-            <h3>${title}</h3>
+            <h3>${titleName}</h3>
             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
@@ -51,7 +62,7 @@ function showMovies(movies) {
 
 //to get the vote colours
 function getClassByRate(vote) {
-    if (vote >= 8) {
+    if (vote >= 7.5) {
         return "green";
     } else if (vote >= 5) {
         return "orange";
